d0ecc48d9c8eb6ec22dc342b8c426d51d014e87e
dmitrijkotov634 <dmitrijkotov634@gmail.com>
2020-10-26 09:14:12 +0300

    delete .bat and .ps1

--- resources/Install-db0mb3r.bat
+++
@@ -1,2 +0,0 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/crinny/db0mb3r/master/resources/Install-db0mb3r.ps1', 'Install-db0mb3r.ps1')"
-powershell -NoProfile -ExecutionPolicy Unrestricted -File Install-db0mb3r.ps1
