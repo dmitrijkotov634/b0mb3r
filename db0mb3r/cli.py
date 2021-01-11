7e2252b88aa8cee402df4deba1f724d65003f721
dmitrijkotov634 <dmitrijkotov634@gmail.com>
2021-01-11 23:23:02 +0300

    Added auto update

--- db0mb3r/cli.py
+++ db0mb3r/cli.py
@@ -13,18 +13,40 @@ from db0mb3r.app.main import app
 from db0mb3r.service import prepare_services
 from db0mb3r.utils import open_url
 
+from requests import get
+from os import system
+
+import pip
 
 @logger.catch
 @click.command()
 @click.option("--ip", default="127.0.0.1")
 @click.option("--port", default=8080)
 @click.option("--only-api", "only_api", is_flag=True, default=False)
-def main(ip: str, port: int, only_api: bool = False):
+@click.option("--disable-updates", "disable_updates", is_flag=True, default=False)
+def main(ip: str, port: int, only_api: bool = False, disable_updates: bool = False):
     if sys.platform == "win32":
         loop = asyncio.ProactorEventLoop()
         asyncio.set_event_loop(loop)
 
     app.state.only_api = only_api
+    
+    if not disable_updates:
+        try:
+            logger.info("Checking for updates")
+            version = pkg_resources.get_distribution("db0mb3r").version
+            updates = get("http://dmitry.darkhost.pro/db0mb3r.version", timeout=7)
+            if updates.status_code == 200:
+                if version == updates.text:
+                    logger.success("No update required")
+                else:
+                    logger.info("Downloading an update using pip")
+                    pip.main(["install", "--upgrade", "db0mb3r"])
+                    logger.success("db0mb3r updated, changes will take effect after restart")
+            else:
+                logger.error("db0mb3r service could not provide the latest version")
+        except:
+            logger.error("Failed to check the latest updates")
 
     prepare_services()
 
