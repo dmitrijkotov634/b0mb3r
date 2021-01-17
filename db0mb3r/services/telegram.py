1528625076ef0d3b28e1afac4dca06a319ae5e69
dmitrijkotov634 <dmitrijkotov634@gmail.com>
2021-01-17 15:52:57 +0300

    add telegram.py

---
+++ db0mb3r/services/telegram.py
@@ -0,0 +1,9 @@
+from db0mb3r.services.service import Service
+
+class telegram(Service):
+
+    async def run(self):
+        await self.post(
+            "https://my.telegram.org/auth/send_password",
+            data={'phone': '+' + self.formatted_phone},
+        )
